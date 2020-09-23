187826838fd5652dfbca5e979c34d2e0dd272003
mvabf <mvabf_dev@outlook.com>
2020-09-23 15:49:01 -0300

    rotas criadas, fazer testes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { response, json } = require('express');
+const { response, json, request } = require('express');
 const express = require('express');
 
 const { uuid } = require('uuidv4');
@@ -7,8 +7,51 @@ const app = express();
 
 app.use(express.json());
 
+const repositories = [];
+
 app.get('/repositories', (request, response) => {
-    return response.json({ message: 'Get funcionando'});
+    return response.json(repositories);
+});
+
+app.post('/repositories', (request, response) => {
+    const { title, url, techs } = request.body;
+
+    const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+    repositories.push(repository);
+
+    return response.json(repository);
+});
+
+app.put('/repositories/:id', (request, response) => {
+    const { id } = request.params;
+    const { title, url, techs } = request.body;
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if (repositoryIndex < 0) {
+        return response.status(400).json({ error: 'Repository not found.' });
+    }
+
+    const repository = { id, title, url, techs, likes: repositories[repositoryIndex].likes };
+
+    repositories[repositoryIndex] = repository;
+
+    return response.json(repository);
+});
+
+app.delete('/repositories/:id', (request, response) => {
+    const { id } = request.params;
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if (repositoryIndex < 0) {
+        return response.status(400).json({ error: 'Repository not found.' });
+    }
+
+    repositories.splice(repositoryIndex, 1);
+
+    return response.status(204).send();
 });
 
 app.listen(3333, () => {
